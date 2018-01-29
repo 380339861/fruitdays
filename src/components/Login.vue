7a2eec5fd1e76978dfc6dcd2189417213e8b604f
bahuang <719321983@qq.com>
2018-01-29 21:34:13 +0800

    first login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,55 +1,51 @@
 <template>
 	<div id="reg">
 		
-			<Header>手机号快捷注册</Header>
-			
-			<div className="reg">
-				<List>
-				
-		         	<i className="iconfont icon-shouji1"></i>
-		          <i className="iconfont icon-yuechi"></i>
-		          
-		        </List>	
-		        <p class="tips">首次用手机号登录将自动为您注册，并有豪礼相送。</p>
-		        <button onClick={()=>this.login()}>登陆</button>
-		        <a href="javascript:;" class="mc_a1">账号密码登录</a>
-		        <a href="javascript:;" class="mc_a2">未收到验证码?</a>
-			</div>
-			
+		<div class="tou">
+			<i class="iconfont icon-jiantou"></i>
+			<p>手机号快捷登录</p>
 		</div>
-	
-	
+		
+		<div class="reg">
+			<li>
+				<i class="iconfont icon-shouji1"></i>
+				<input type="number" placeholder="手机号" />
+				<button>获取验证码</button>
+			</li>
+			<li>
+				<i class="iconfont icon-yuechi"></i>
+				<input type="number" placeholder="短信验证码" />
+			</li>
+			<p class="p1">首次用手机号登陆将自动为您注册，并有豪礼相送。</p>
+			<input type="button" value="登陆" />
+			<p class="p2">账户密码登录</p>
+			<p class="p3">未收到验证码？</p>
+		</div>
+		
+	</div>
 </template>
 
 <script>
-	export default{
-	name : "login",
-	data : function(){
-		return {
-			
-		}
-	},
-}
-	
-	
-	
 </script>
 
-
-
-    
-
 <style scoped>
-#reg .reg{margin-top: 120px;}
+@import url("//at.alicdn.com/t/font_491734_5q9bhssv2t9.css");
+	
+#reg{width: 100%;height: 190px;background: url(https://wap.fruitday.com/content/images/me/login-bg.png) no-repeat fixed 0 0;background-size: 100% auto;color: #000000;}
+li{list-style: none;}
 
-#reg .reg span{display: inline-block; width: 70px;height: 25px; margin-left: 10px; border-radius: 2px;background: #f93;line-height: 25px;font-size: 12px; color: #fff;text-align: center;}
-#reg .reg i{color: #000;font-size: 16px;margin-left: 20px;width: 30px;}
-#reg .reg button{width: 80%;height: 40px; margin: 80px 30px 10px;border-radius: 10px;background: #f93;line-height: 40px;font-size: 16px; color: #fff;text-align: center;border: none;}
-#reg .reg .tips{height:20px;line-height: 20px;font-size: 10px;color: #878787;margin-left: 40px;margin-top: 20px;}
-#reg .reg a{color: #f93;font-size: 12px;margin: 10px 42px;}
+.tou{height: 50px;color: #75a739;font-size: 20px;line-height: 50px;}
+.tou i{margin-left: 20px;font-weight: bolder;}
+.tou p{display: inline-block;margin-left: 70px}
 
-
-
-#reg .am-list-item am-input-item am-list-item-middle{border:none}
-
+.reg{margin-top: 140px;padding: 20px;color: #878787;}
+.reg li{border-bottom:1px solid #d8d8d8 ;height: 50px;line-height: 50px;}
+.reg li i{font-size: 20px;margin: 0 10px;}
+.reg li input{border:none;background: rgba(0,0,0,0);width: 180px;height: 30px;outline: none;color: #878787;}
+.reg li button{display: inline-block;width: 80px;height: 30px;margin-left: 10px;border-radius: 2px;background: #f93;line-height: 25px;font-size: 12px;color: #fff;text-align: center;border: none;}
+.reg .p1{margin-top: 15px;font-size: 12px;}
+.reg>input{display: block;width: 100%;
+height: 40px;margin: 50px 0 15px;border-radius: 2px;background: #f93;line-height: 40px;font-size: 16px;color: #fff;text-align: center;border: none;}
+.reg .p2,.reg .p3{display: inline-block;width: 49%;color: #f93;}
+.reg .p3{text-align: right;}
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,82 @@
+<template>
+	<div id="div">
+    	<div class="mc_userheader">
+    		<img src="https://wap.fruitday.com/content/images/me/old_v0.png" />
+    		<p>18335450635</p>
+    		<h6>签到送好礼</h6>
+    		<i class="iconfont icon-erweima"></i>
+    		<span>会员码</span>
+	    	<ul>
+	    		<li>积分<p>0</p></li>
+	    		<li>余额<p>0</p></li>
+	    		<li>优惠券<p>3</p></li>
+	    		<li>礼品<p>0</p></li>
+	    	</ul>
+    	</div>
+    	
+    	<div class="mc_usercont">
+    	
+    			<div class="mc_one">
+    				<i class="iconfont icon-dingdan"></i>
+    				<p>我的订单</p>
+    				<span>全部订单<i class="iconfont icon-jiantou1"></i></span>
+    			</div>
+    			
+    			<ul class="mc_two">
+    				<li><i class="iconfont icon-zhifu"></i><p>待付款</p></li>
+    				<li><i class="iconfont icon-fahuo"></i><p>待发货</p></li>
+    				<li><i class="iconfont icon-fahuo1"></i><p>待收货</p></li>
+    				<li><i class="iconfont icon-pingjia"></i><p>待评价</p></li>
+    				<li><i class="iconfont icon-tuihuanhuo"></i><p>退换货</p></li>
+    			</ul>
+    			
+    			<div class="mc_one">
+    				<i class="iconfont icon-dingdan"></i>
+    				<p>会员中心</p>
+    				<span>享等级特权<i class="iconfont icon-jiantou1"></i></span>
+    			</div>
+    			
+    			<div class="mc_one">
+    				<i class="iconfont icon-dingdan"></i>
+    				<p>企业购</p>
+    			</div>
+    		
+    			<div class="mc_one">
+    				<i class="iconfont icon-dingdan"></i>
+    				<p>在线客服</p>
+    			</div>
+    		
+    		</div>	
+    		
+    	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+@import url("//at.alicdn.com/t/font_491734_5q9bhssv2t9.css");
+
+.mc_userheader{width: 100%;height: 190px;background: url(https://wap.fruitday.com/content/images/me/login-bg.png) no-repeat fixed 0 0;background-size: 100% auto;color: #000000;}
+.mc_userheader img{height: 18px;padding-left: 50px;margin-top: 70px;}
+.mc_userheader>p{font-size: 18px;margin-left: 80px;margin-top: -48px;}
+.mc_userheader h6{font-size: 10px; height: 16px;width: 70px;background-color:#ff8000 ;line-height: 18px;border-radius: 10px;color: #fff;text-align: center;margin-left: 80px;margin-top: 10px;}
+.mc_userheader>i{font-size: 40px;position: absolute;left: 265px;top: 40px;color:#878787 ;}
+.mc_userheader>span{font-size: 10px;position: absolute;left: 269px;top: 82px;color:#878787 ;}
+.mc_userheader ul{display: flex;color: #000000;background: url(https://wap.fruitday.com/content/images/me/login-bg.png) no-repeat fixed 0 0;background-size: 100% auto;line-height: 24px;
+margin-top: 40px;font-size: 13px;}
+.mc_userheader ul li{flex: 1;text-align: center;color:#3a3a3a}
+.mc_userheader ul li p{color:#ff8000 ;font-weight: bolder;font-size: 14px;}
+
+.mc_usercont{background: url(https://wap.fruitday.com/content/images/me/login-bg.png) no-repeat fixed 0 0;}
+.mc_usercont .mc_one{height: 40px;line-height: 40px;font-size: 14px;color: #3a3a3a;border-bottom: 1px solid #eee;background-color: rgba(255,255,255,0.9);}
+.mc_usercont .mc_one i{padding:0 10px;font-size: 18px;}
+.mc_usercont .mc_one p{display: inline-block;}
+.mc_usercont .mc_one span{color: #878787;font-size: 12px;float: right;}
+.mc_usercont .mc_one span i{font-size: 14px;}
+
+.mc_usercont .mc_two{display: flex;width: 100%;background-color: rgba(255,255,255,0.9);height: 80px;color: #3a3a3a;margin-bottom: 10px;}
+.mc_usercont .mc_two li{display: flex;flex: 1;flex-direction: column;text-align: center;line-height: 30px;padding-top: 15px;}
+.mc_usercont .mc_two li:last-child i{font-size: 20px;}
+
+</style>
